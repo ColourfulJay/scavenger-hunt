61b4c8c8468b9e0053dfa0f7dc5bf71587aaa37f
ColourfulJay <telferlily2@gmail.com>
2023-04-24 11:39:49 -0500

    Add project files.

---
+++ scavenger-hunt/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("The last digits of the last year of numbered treaties(X), and find the Xst number of the treaty ABCs");
+string name = Console.ReadLine();
+Console.WriteLine("Hello, " + name + "!");
+
